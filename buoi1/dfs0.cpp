9f6540a533a190a3451ec950883e8ddf0169c344
longnhat2k9 <phannguyenlongnhat2k9@gmail.com>
2026-09-17 15:53:53 +0700

    Exercise 1: DFS0

---
+++ buoi1/dfs0.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int d[100000];
+long long n, k;
+vector<long long>adj[100000];
+
+void dfs(long long u)
+{
+	for(auto &v : adj[u])
+	{
+		d[u] = 1;
+		if(d[v] == 0) dfs(v);
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	memset(d, 0, sizeof(d));
+
+	cin >> n >> k;
+	long long u, v;
+	for(long long i = 0; i < k; i++)
+	{
+		cin >> u >> v;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+
+	d[1] = 1;
+	dfs(1);
+
+	for(long long i = 1; i <= n; i++)
+	{
+		if(d[i] == 1) cout << i << " ";
+	}
+}
---
+++ buoi1/dfs0.md
@@ -0,0 +1,37 @@
+Cho đồ thị vô hướng G có $n$ đỉnh từ $1$ đến $n$ và $m$ cạnh nối đỉnh $i$ và $j$ $(1 \le n, m \le 1000, 1 \le i, j \le n)$. Liệt kê tất cả các đỉnh từ $1$ có thể đi tới nó.
+
+## Example Input 1
+```
+6 8
+1 2
+1 3
+1 4
+2 4
+3 4
+3 5
+3 6
+5 6
+```
+
+## Example Output 1
+```
+1 2 3 4 5 6
+```
+
+## Example Input 2
+```
+10 8
+1 2
+2 4
+2 5
+4 8
+4 9
+3 7
+3 6
+6 10
+```
+
+## Example Output 2
+```
+1 2 4 5 8 9
+```
