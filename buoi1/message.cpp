94fe2c1883b420c1d4b90322bd056d86b53472ad
longnhat2k9 <phannguyenlongnhat2k9@gmail.com>
2026-09-25 14:56:45 +0700

    Update Sol for problem message

--- buoi1/message.cpp
+++ buoi1/message.cpp
@@ -1,16 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int d[801];
-long long n, k;
-vector<long long>adj[801];
+long long n, m;
+long long cnt = 0;
+vector<long long>adj[1000];
+long long d[1000];
 
 void dfs(long long u)
 {
-	d[u] = 1;
+	d[u] = cnt;
 	for(auto &v : adj[u])
 	{
-		if(d[v] == 0) dfs(v);
+		if(d[v] != cnt) dfs(v);
 	}
 }
 
@@ -18,47 +19,29 @@ int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	memset(d, 0, sizeof(d));
-
-	cin >> n >> k;
+	cin >> n >> m;
 	long long u, v;
-	for(long long i = 0; i < k; i++)
+	for(long long i = 0; i < m; i++)
 	{
 		cin >> u >> v;
 		adj[u].push_back(v);
 	}
 
-	// for(auto &x : adj) sort(x.begin(), x.end());
-
-	long long cnt = 0, mn = 1e18;
-	for(long long j = 1; j <= n; j++)
+	for(long long i = 1; i <= n; i++)
 	{
-		// cout << j << endl;
-		cnt = 0;
-		memset(d, 0, sizeof(d));
-		for(long long i = j; i <= n; i++)
+		if(d[i] == 0)
 		{
-			if(d[i] == 0)
-			{
-				cnt++;
-				dfs(i);
-				// for(long long j = 1; j <= n; j++) cout << d[j] << " ";
-				// cout << endl;
-			}
+			cnt++;
+			dfs(i);
 		}
-		for(long long i = 1; i < j; i++)
-		{
-			if(d[i] == 0)
-			{
-				cnt++;
-				dfs(i);
-				// for(long long j = 1; j <= n; j++) cout << d[j] << " ";
-				// cout << endl;
-			}
-		}
-		mn = min(mn, cnt);
 	}
-	
 
-	cout << mn;
+	set<long long>dc;
+
+	for(long long i = 1; i <= n; i++)
+	{
+		dc.insert(d[i]);
+	}
+
+	cout << dc.size();
 }
